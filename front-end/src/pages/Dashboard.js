648358ffafd4d9294d83f069d9e29c7fa6ec61a0
Anastazja <anastazjagaluza@gmail.com>
2020-11-25 13:46:10 +0100

    mailer works with the preview

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Dashboard() {
+    const [emailData, updateEmailData] = useState({from: "", to: "", subject: "", text: ""})
+    const [response, setResponse] = useState();
+    async function sendMail(e) {
+        e.preventDefault();
+        console.log(emailData);
+        const res = await fetch("http://localhost:8080/mailer", {method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(emailData)})
+        const resp = await res.json();
+        console.log(resp);
+        setResponse(resp.PreviewURL);
+    }
+
     return (
         <>
-        <p>Hi!</p>
+        <p>Hi! From here you can send your email!</p>
+        <form onSubmit={e => sendMail(e)} style={{display: "flex", flexDirection: "column", width: "40vw", marginLeft: "30vw"}}>
+            <input onChange={(e) => updateEmailData({...emailData, from: e.target.value})} type="email" required placeholder="From"></input>
+            <input onChange={(e) => updateEmailData({...emailData, to: e.target.value})} type="email" required placeholder="To"></input>
+            <input onChange={(e) => updateEmailData({...emailData, subject: e.target.value})} type="text" required placeholder="Subject"></input>
+            <textarea onChange={(e) => updateEmailData({...emailData, text: e.target.value})} type="text" required placeholder="Text"></textarea>
+            <button type="submit">Send!</button>
+        </form>
+        {response != null ? <><p>Message sent!</p><a target="_blank" href={response}>Here is your message preview</a></> : null}
         </>
     )
 }
--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const auth = require("./auth");
-
+const main = require("./mailer");
 app.use(auth);
+app.use(express.json());
+
+
+app.post("/mailer", async (req, res) => {
+    const emailData = [req.body.from, req.body.to, req.body.subject, req.body.text ];
+    const response = await main(...emailData).catch(console.error);
+    res.status(200).json(response);
+})
 
 app.listen("8080", ()=>{
     console.log("Listening on the port 8080");
 })
+
